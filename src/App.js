be26da547b140f0d77b1013866e0b300a64103ea
Ian Carlos <carlos.ian007@gmail.com>
2020-01-10 09:56:19 -0200

    Add redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 
-import Routes from './routes';
+import './config/ReactotronConfig';
+
 import GlobalStyle from './styles/global';
+import Routes from './routes';
 
 import history from '~/services/history';
 
---
+++ src/store/createStore.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+
+export default (rootReducer, middlewares) => {
+  const enhancer =
+    process.env.NODE_ENV === 'development'
+      ? compose(applyMiddleware(...middlewares), console.tron.createEnhancer())
+      : applyMiddleware(...middlewares);
+
+  return createStore(rootReducer, enhancer);
+};
